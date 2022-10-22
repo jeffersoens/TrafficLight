79806aef53c40277ca70968b51dfb33bf40608fe
Vyacheslav Gorbatenko <jeffersoens@gmail.com>
2022-10-22 13:21:58 +0300

    Added views and a button. Configured app's logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var currentLightView: UIView?
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redLightView: UIView!
+    
+    @IBOutlet var yellowLightView: UIView!
+    
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var toggleButton: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        redLightView.layer.cornerRadius = 55
+        yellowLightView.layer.cornerRadius = 55
+        greenLightView.layer.cornerRadius = 55
+        
+        redLightView.alpha = 0.35
+        yellowLightView.alpha = 0.35
+        greenLightView.alpha = 0.35
+        
+        toggleButton.layer.cornerRadius = 15
     }
-
-
+        
+    @IBAction func toggleLights() {
+        
+        toggleButton.setTitle(
+            "Next light",
+            for: .normal
+        )
+        
+        switch currentLightView {
+        case yellowLightView:
+            redLightView.alpha = 0.35;
+            yellowLightView.alpha = 1;
+            greenLightView.alpha = 0.35;
+            currentLightView = greenLightView
+        case greenLightView:
+            redLightView.alpha = 0.35;
+            yellowLightView.alpha = 0.35;
+            greenLightView.alpha = 1;
+            currentLightView = redLightView
+        default:
+            redLightView.alpha = 1;
+            yellowLightView.alpha = 0.35;
+            greenLightView.alpha = 0.35;
+            currentLightView = yellowLightView
+        }
+    }
 }
-
